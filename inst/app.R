020f03f2b98f7edfb0209df814e88644351a065f
Juan Pablo Marin Diaz <jpmarindiaz@gmail.com>
2020-08-02 12:26:33 -0500

    add sample app

---
+++ inst/app.R
@@ -0,0 +1,45 @@
+library(shiny)
+library(makeup)
+library(homodatum)
+library(hgchmagic)
+
+dat <- homodatum:::Dat("04/21?2020?")
+makeup(as.Date(dat), locale = "de-DE")
+
+ui <- fluidPage(
+  selectInput("locale", "Locale", choices = c("en-US", "es-ES","de-DE")),
+  dateInput("date", "Some date", value = "2020-04-21"),
+  verbatimTextOutput("formatted_date"),
+  textInput("custom_date", "Some date", value = "04/21?2020?"),
+  verbatimTextOutput("formatted_custom_date"),
+  hr(),
+  highchartOutput("chart"),
+  verbatimTextOutput("debug")
+)
+
+d1 <- data.frame(fecha = c("2020/05/03","2020/05/04"),
+                 vals = 1:2)
+
+
+server <- function(input, output, session){
+
+  output$formatted_date <- renderPrint({
+    makeup(as.Date(input$date), locale = input$locale)
+  })
+
+  output$formatted_custom_date <- renderPrint({
+    dat <- homodatum:::Dat(input$custom_date)
+    makeup(as.Date(dat), locale = input$locale)
+  })
+
+  output$chart <- renderHighchart({
+    hgch_line_DatNum(d1)
+  })
+
+  output$debug <- renderPrint({
+    d1
+  })
+
+}
+
+shinyApp(ui, server)
